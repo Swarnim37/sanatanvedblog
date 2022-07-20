4df6ff7ab64f10b0a69e241dd10276485ded46db
Swarnim37 <swarnimsharma37@gmail.com>
2022-07-20 18:29:10 +0200

    fix35

--- components/AdjacentPostCard.jsx
+++ components/AdjacentPostCard.jsx
@@ -4,10 +4,6 @@ import Link from "next/link";
 
 const AdjacentPostCard = ({ post, position }) => (
   <>
-    <div
-      className="absolute rounded-lg bg-center bg-no-repeat bg-cover shadow-md inline-block w-full h-72"
-      style={{ backgroundImage: `url('${post.featuredImage}')` }}
-    />
     <div className="absolute rounded-lg bg-center bg-gradient-to-b opacity-50 from-gray-400 via-gray-700 to-black w-full h-72" />
     <div className="flex flex-col rounded-lg p-4 items-center justify-center absolute w-full h-full">
       <p className="text-white text-shadow font-semibold text-xs">
